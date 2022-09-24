83e40aa6deb170c2297c9e2d6a4418667e3a521e
Luis Fernando <luis.nando.acarau@gmail.com>
2022-09-24 01:23:56 -0300

    Layout Login

---
+++ src/components/input/InputData.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <label for=""></label><br>
+        <input type="text">
+    </div>
+</template>
+<script>
+export default {
+    name:"InputData",
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+<style>
+
+</style>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,21 +1,36 @@
 <template>
     <div class="box-login">
-        <h1>{{mensagem}}</h1>
+        <div class="logo">
+            <h1>Login</h1>
+           
+        </div>
+        <InputData></InputData>
     </div>
 </template>
 
 <script>
-export default {
-    name : "LoginView",
-    data(){
-        return{
-          mensagem:"Estou na tela de Login"
-      }
-    }
-    
+import InputData from "@/components/input/InputData.vue";
+export default{
+    name: "LoginView",
+    comments: {
+        InputData
+    },
+    components: { InputData }
 }
 </script>
 
 <style scoped>
+
+    .box-login{
+        width: 300px;
+        margin: auto;
+        margin-top: 70px;
+        border: 1px solid #edd;
+    }
+
+    .logo{
+        text-align: center;
+        color: var(--cor-primaria);
+    }
 
 </style>
